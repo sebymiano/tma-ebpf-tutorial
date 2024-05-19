384cd7e1677bbc04bef4863fb743fa78068d6e14
Angelo Tulumello <angelo.tulumello@gmail.com>
2024-05-19 12:25:27 +0200

    update tracepoint

--- src/system/ebpf/ebpf_tutorial_tracepoint.bpf.c
+++ src/system/ebpf/ebpf_tutorial_tracepoint.bpf.c
@@ -5,7 +5,7 @@
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
-int __always_inline compare_11_byte_strings(char *str1, const char str2[11]) {
+/*int __always_inline compare_11_byte_strings(char *str1, const char str2[11]) {
 #pragma clang loop unroll(full)
     for (int i = 0; i < 11; i++) {
         if (str1[i] != str2[i]) {
@@ -13,7 +13,7 @@ int __always_inline compare_11_byte_strings(char *str1, const char str2[11]) {
         }
     }
     return 0; // strings are equal
-}
+}*/
 
 SEC("tracepoint/syscalls/sys_enter_openat")
 int tracepoint__syscalls__sys_enter_openat(struct trace_event_raw_sys_enter *args) {
@@ -23,13 +23,12 @@ int tracepoint__syscalls__sys_enter_openat(struct trace_event_raw_sys_enter *arg
 
     bpf_probe_read_user_str(fname, sizeof(fname), filename);
 
-    if (compare_11_byte_strings(fname, "/etc/passwd") == 0) {
+    if (bpf_strncmp(fname, 11, "/etc/passwd") == 0) {
         // print to /sys/kernel/debug/tracing/trace_pipe
         bpf_printk("[!] PID %d tried to open /etc/passwd\n",
                    bpf_get_current_pid_tgid() >> 32);
 
         bpf_probe_write_user((char *)args->args[1], "/dev/null", 10);
-        // bpf_send_signal(9);
     }
 
     return 0;
